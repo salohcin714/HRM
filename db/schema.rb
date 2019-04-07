7b0f9acd06b901aae5f60d6461b0b3f83f52d6b2
Nicholas Norris <41271531+salohcin714@users.noreply.github.com>
2019-04-07 18:31:58 -0400

    Switch to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20190311174622) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "employees", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
